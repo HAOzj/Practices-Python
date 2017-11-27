13cfc32a90f9e744260126633e9151d668a78de9
HAO Zhaojun <haozhaojun@eisti.eu>
2017-11-27 18:39:21 +0800

    conform to pip8 comments standard

--- ergodicity.py
+++ ergodicity.py
@@ -5,11 +5,18 @@ Created on Sunday 13th Aug 2017
 
 '''
 
-# m 全部可能状态的数目
-# k 有放回的抽取的次数
-# g 要经历的状态的最小数目
-# m <= k, k>2
 def ergo(m,k,g):
+	'''function to caculate the probability to pass at least g states when 
+	We uniformly randomly jump onto a state amongs m kind of stateseach time.
+	After k times jumps, how possible are we to pass at least g states ?
+	@pamameters : 
+		m(int) :- 全部可能状态的数目
+		k(int) :- 有放回的抽取的次数
+		g(int) :- 要经历的状态的最小数目
+		m <= k, k>2
+	@return :
+		n(int) :- the possibility multplied by m**k 
+	'''
 	liste = [ i for i in range(m)]
 	SS = [[a,b] for a in liste for b in liste]
 
@@ -17,10 +24,10 @@ def ergo(m,k,g):
 	for i in range(k-2):
 		SS = [ a+[b] for a in SS for b in liste]
 
-	# k 纪录符合要求的状态的数目	
-	k = 0
+	# n 纪录符合要求的状态的数目	
+	n = 0
 	for i in SS :
 		if(len(set(i))>= g):
-			k+=1
-	return(k)
+			n+=1
+	return(n)
 	
