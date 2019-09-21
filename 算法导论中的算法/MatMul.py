aeed4dde2e7b66d0a1730c1c3963d7c7a0e9646a
haozhaojun <haozhaojun@haozhaojundeMacBook-Pro.local>
2019-09-21 09:27:57 +0800

    reformatting

--- 算法导论中的算法/MatMul.py
+++ 算法导论中的算法/MatMul.py
@@ -1,37 +1,38 @@
 """
 Created on Thu July 14:35 2017
 
-Author : HAO zhaojun
+@author: woshihaozhaojun@sina.com
 """
+
+
 def MatMulOrder(D):
-	'''the function to optimise the order of matrix-chain multiplication
+	"""optimise the order of matrix-chain multiplication
 
-	Args :
-		input(iterables) :- an array of matrix dimension, 
-							ith and (i+1)th represent the ith matrice dimension 
-	Returns : 
-		M(iterables) :- matrice to record the number of multiplication at element level, 
+	Args:
+		D(iterables) :- an array of matrix dimension,
+							ith and (i+1)th represent the ith matrix dimension
+	Returns:
+		M(iterables) :- matrix to record the number of multiplication at element level,
 						its [i][j] element represents the number of scalar multiplication 
-						between ith matrice and jth matrice 
-		P(iterables) :- matrice to records the index of left matrice in last multiplication, 
-						its [i][j] element represents the index of left matrice in last multiplication 
-						between ith matrice and jth matrice
-	'''
+						between ith matrix and jth matrix
+		P(iterables) :- matrix to records the index of left matrix in last multiplication,
+						its [i][j] element represents the index of left matrix in last multiplication
+						between ith matrix and jth matrix
+	"""
 	num = len(D)-1 # number of matrix in the chain
-	print("There are %d matrix to multiply" %num )  
-	M = [ [0 for j in range(num)] for i in range(num) ] 
-	P = [ [0 for j in range(num)] for i in range(num) ]
+	print(f"There are {num} matrix to multiply")
+	M = [[0 for _ in range(num)] for _ in range(num)]
+	P = [[0 for _ in range(num)] for _ in range(num)]
 
-	for i in range(num-2, -1, -1 ): # i要从大到小
-		for j in range(i+1, num): # i == j时, M[i][j]=0,所以不用更新
-			M[i][j]= 100000000 # i-th矩阵到j-th矩阵的乘的最优值初始化为inf
-			for k in range(i,j) : 
+	# i要从大到小
+	# i == j时, M[i][j]=0,所以不用更新
+	# i-th矩阵到j-th矩阵的乘的最优值初始化为inf
+	for i in range(num-2, -1, -1):
+		for j in range(i+1, num):
+			M[i][j] = 100000000
+			for k in range(i, j):
 				new = M[i][k] + M[k+1][j] + D[i]*D[k+1]*D[j+1]
-				if(new < M[i][j]):
+				if new < M[i][j]:
 					M[i][j] = new 
 					P[i][j] = k
-	return(M,P)
-						
-						
-						
-	
+	return M, P
--- 算法导论中的算法/OBST.py
+++ 算法导论中的算法/OBST.py
@@ -3,10 +3,30 @@ Created on the 15th July 2018
 
 @author : woshihaozhaojun@sina.com
 """
-import numpy as np 
+import numpy as np
 
-def OBST(P,Q):
-    ''' 最优二叉搜索树
+
+def OBST(P, Q):
+    """最优二叉搜索树
+
+    假定我们正在设计一个程序，实现英文文本到法语的翻译。对英语文本中出现的每个单词,
+    我们需要查找对应的法语单词。为了实现这些查找操作，
+    我们可以创建一棵二叉搜索树,将n个英语单词作为关键字，对应的法语单词作为关联数据。
+    由于对文本中的每个单词都要进行搜索，我们希望花费在搜索上的总时间尽量少。
+    因为在二叉搜索树中搜索一个关键字需要访问的结点数等于包含关键字的结点的深度加1.
+    我们希望文本中频繁出现的单词被置于靠近根的位置.
+    而且,文本中的一些单词可能没有对应的法语单词，这些单词根本不应该出现在二叉搜索树中.
+    在给定单词出现频率的前提下，如何组织一棵二叉搜索树，使得所有搜索操作访问的结点总数最少?
+    这就是最优二叉搜索树问题.
+
+    形式定义如下：
+    给定一个n个不同关键字的已排序的序列K=<k1, k2, ..., kn>.
+    对每个关键字ki,都有一个概率pi表示其搜索频率。
+    有些要搜索的值可能不在K中，因此我们还有n+1个伪关键字,do,d1,...,dn表示不在K中的值.
+    d0表示所有小于k1的值,dn表示所有大于kn的值,对i=1,2,...,n-1，伪关键字di表示所有在ki和k i+1 之间的值。
+    对每个伪关键字di，都有一个概率qi表示对应的搜索频率.
+    每个关键字ki是一个内部节点,而每个伪关键字di是一个叶结点。
+    每次搜索要么成功（找到某个关键字ki）要么失败（找到某个伪关键字di）
 
     Args:
         P(iterables) :- 关键字的概率
@@ -15,76 +35,75 @@ def OBST(P,Q):
     Returns:
         Exp(dict) :- Exp[i_j]表示i-th关键字到j-th关键字所在的子树的路径长度期望
         Pivot(dict) :- Pivot[i_j]表示i-th关键字到j-th关键字所在的子树的根节点
-    '''
+    """
     n = len(P)
 
-    try :
+    try:
         assert len(Q) == n+1
     except AssertionError:
         print("P的长度为{},Q应该长{},但实际长{}".format(
             n, n+1, len(Q)
         )     
         )
-    Exp = dict()
+    exp_map = dict()
     
-    Pivot = dict()
+    pivot_map = dict()
     
-    for i in range(n,-1,-1):
-        Exp["{}_{}".format(i,i-1)] = 0
-        Pivot["{}_{}".format(i,i)] = i
+    for i in range(n, -1, -1):
+        exp_map[f"{i}_{i-1}"] = 0
+        pivot_map[f"{i}_{i}"] = i
         for j in range(i, n):
-            
-            if i==j:
-                Exp["{}_{}".format(i,j)] = P[i] 
-            else :
+            if i == j:
+                exp_map[f"{i}_{j}"] = P[i]
+            else:
                 optimum = np.inf
                 pivot = i+1
                 for k in range(i+1, j+1):
-                    new = Exp["{}_{}".format(i,k-1)] + Exp["{}_{}".format(k+1, j)] +  sum(P[i:j+1]) 
-                    if optimum > new :
+                    new = exp_map[f"{i}_{k-1}"] +\
+                          exp_map[f"{k+1}_{j}"] +\
+                          sum(P[i:j + 1])
+                    if optimum > new:
                         optimum = new
                         pivot = k
-                Exp["{}_{}".format(i,j)] = optimum
-                Pivot["{}_{}".format(i,j)] = pivot
+                exp_map[f"{i}_{j}"] = optimum
+                pivot_map[f"{i}_{j}"] = pivot
 
-    root = Pivot["{}_{}".format(0, n-1)]
-    print("k{}是根".format( root    ))
+    root = pivot_map[f"{0}_{n-1}"]
+    print(f"k{root}是根")
     
-    relation_recur(Pivot,0, root-1, 'l')
-    relation_recur(Pivot, root+1, n-1, 'r')
-    
+    relation_recur(pivot_map,0, root-1, 'l')
+    relation_recur(pivot_map, root+1, n-1, 'r')
                  
-    return Exp, Pivot
-                
-def relation_recur(Pivot, i, j, pos):
-    ''' 通过Pivot字典来文字输出树结构
+    return exp_map, pivot_map
+
+
+def relation_recur(pivot, i, j, pos):
+    """通过Pivot字典来文字输出树结构
     
     Args:
-        Pivot(dict) :- Pivot[i_j]表示i-th关键字到j-th关键字所在的子树的根节点
+        pivot(dict) :- Pivot[i_j]表示i-th关键字到j-th关键字所在的子树的根节点
         i(int) :- 子树包含的最小关键字的索引
         j(int) :- 子树包含的最大关键字的索引
         pos(str) :- i-th到j-th关键所在的子树相对上级子树的位置,
                     choices =["l", "r"]
-    '''
+    """
     if j-i >= 0:
-        pivot = Pivot["{}_{}".format(i,j)]
-        if pos =='r':
-            print("k{}的右孩子是k{}".format(i-1, pivot))
-        else :
-            print("k{}的左孩子是k{}".format(j+1, pivot))
+        pivot = pivot[f"{i}_{j}"]
+        if pos == "r":
+            print(f"k{i-1}的右孩子是k{pivot}")
+        else:
+            print(f"k{j+1}的左孩子是k{pivot}")
         
-        relation_recur(Pivot, i, pivot-1, 'l')
-        relation_recur(Pivot, pivot+1, j, 'r')
+        relation_recur(pivot, i, pivot - 1, 'l')
+        relation_recur(pivot, pivot + 1, j, 'r')
+
 
 def main():
-    P = [0.15, 0.10, 0.05, 0.10, 0.20] # 5
-    Q = [0.05, 0.10, 0.05, 0.05, 0.05, 0.10] # 6
+    p = [0.15, 0.10, 0.05, 0.10, 0.20] # 5
+    q = [0.05, 0.10, 0.05, 0.05, 0.05, 0.10] # 6
 
-    OBST(P, Q)
+    OBST(p, q)
+
 
 if __name__ == "__main__":
     main()
-
-
-    
-    
